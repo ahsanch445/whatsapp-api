92eb784b01a9506a40395445fade083aab82b2b9
Ahsan Ch <ach374392@gmail.com>
2024-01-30 21:01:18 +0500

    commit

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ var usersRouter = require('./routes/users');
 const cors = require("cors")
 var app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://what-app-sand.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
 app.use(cors())
 
 // view engine setup
