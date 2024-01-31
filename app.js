36cd9cfb531edfb8d4972bc33633f6685bc90560
Ahsan Ch <ach374392@gmail.com>
2024-01-31 17:45:49 +0500

    fix cors

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const cors = require("cors")
 var app = express();
-
+app.use(cors())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(cors())
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
