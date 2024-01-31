a2f097b3bd24b24f2ffd10f53c1417a37e762dd8
Ahsan Ch <ach374392@gmail.com>
2024-01-31 13:29:23 +0500

    fix cors error

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 });
 
 
-app.use(cors())
+app.use(cors("https://what-app-sand.vercel.app/Upload/File"))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
